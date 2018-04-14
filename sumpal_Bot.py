38e78a2bb51bbf7d7fffa0e06bb37b5d0d299e13
Jacob Jurilla <iamjake@protonmail.com>
2018-04-14 18:06:17 -0400

    Able to recall fixed number of messages. Bot is able to always listen.

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -40,12 +40,32 @@ def botTalkToGroup(bot_ID, message,attach):
     client.bots.post(bot_ID, message)
 
 def main():
+    groupMessages = groupy.api.messages.Messages(client.session,group_ID)
     print("\t\t**Sum.Pal Bot Starting**\n\n")
     botInitialPost(bot_ID,"The Evil Bot has started, current time")
     botInitialPost(bot_ID, str(time.ctime()))
-    for messageText in groupMessages.list(limit=4):
-        print(messageText.text)
+        # for messageText in groupMessages.list():
+        # print(messageText.text)
+    
+    recentMessageID = 0
 
+    for messageText in groupMessages.list():
+        recentMessageID = messageText.id
+        break
+    
+    i = 0
+    while(1):
+        for messageText in groupMessages.list_since(recentMessageID):
+            if "@bot" in messageText.text:
+                print(" - @bot recieved")
+                botTalkToGroup(bot_ID, "I heard you!", [])
+            recentMessageID = messageText.id
+            i += 1
+            if i == 4:
+                for messageText in groupMessages.list(limit=i):
+                    print(messageText.text)
+                i = 0
+            print(i)
 
 if __name__ == "__main__":
     main()

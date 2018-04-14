1d37bee4cfc178e1597a95da161c355c16b0be42
AnthonyLSee <as2242@scarletmail.rutgers.edu>
2018-04-14 17:48:07 -0400

    Revert "Updated bot"
    This reverts commit 63d0c9d4de53b4402561932edff5994f4366663a.

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -26,19 +26,20 @@ def main():
     # Bot listening ---------------------------------#
 
     # Get messages for SPECIFIC group #
-    groupMessages = groupy.api.messages.Messages(client.session,"40061967")
+    groupMessages = groupy.api.messages.Messages(client.session,40061967)
 
     # Get MOST RECENT message ID #
-    recentMessageID = 0
     for messageText in groupMessages.list():
         recentMessageID = messageText.id
         break
 
-    # Bot is now LISTENING #
+
     while(1):
+
+
         for messageText in groupMessages.list_since(recentMessageID):
             if "@bot" in messageText.text:
-                print(" - @bot received")
+                print("         - @bot Found.")
                 botTalkToGroup("22482b2e8f82a16f13242f93d8","I hear you!",[])
             recentMessageID = messageText.id
 

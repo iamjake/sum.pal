9dee4102a00abbbef2fa0d3f580f80d2ce5bbc2d
AnthonyLSee <as2242@scarletmail.rutgers.edu>
2018-04-14 17:48:58 -0400

    Updated bot, added listining based on user command

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -3,8 +3,10 @@ import sys
 import json
 import groupy
 
+
 from groupy.client import Client
-token = "dnQ98hYoQQjFlo5sYAN4mqoCTYLH0V0Ye8cOGvM9"
+from groupy.api import messages
+token = ""
 client = Client.from_token(token)
 
 
@@ -17,13 +19,28 @@ def botTalkToGroup(botID,message,attach):
 def main():
     print("     == sum.Pal Bot ==       \n\n")
 
-    groups = list(client.groups.list_all())
-    for group in groups:
-        print(group.name)
 
     # BOT TALKING TO GROUP #
-    botTalkToGroup("22482b2e8f82a16f13242f93d8","david aint comin",[])
+    #botTalkToGroup("22482b2e8f82a16f13242f93d8","david aint comin",[])
 
+    # Bot listening ---------------------------------#
+
+    # Get messages for SPECIFIC group #
+    groupMessages = groupy.api.messages.Messages(client.session,"40061967")
+
+    # Get MOST RECENT message ID #
+    recentMessageID = 0
+    for messageText in groupMessages.list():
+        recentMessageID = messageText.id
+        break
+
+    # Bot is now LISTENING #
+    while(1):
+        for messageText in groupMessages.list_since(recentMessageID):
+            if "@bot" in messageText.text:
+                print(" - @bot received")
+                botTalkToGroup("22482b2e8f82a16f13242f93d8","I hear you!",[])
+            recentMessageID = messageText.id
 
 
 

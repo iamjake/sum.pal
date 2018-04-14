40f891f66fb4af3ce43d794a1ed2a9052895102f
AnthonyLSee <as2242@scarletmail.rutgers.edu>
2018-04-14 15:03:16 -0400

    Update to bot , Ablitiy to talk to group chat

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -4,11 +4,28 @@ import json
 import groupy
 
 from groupy.client import Client
-
-token = ""
-
+token = "dnQ98hYoQQjFlo5sYAN4mqoCTYLH0V0Ye8cOGvM9"
 client = Client.from_token(token)
 
-for group in client.groups.list_all():
-   print(group.name)
 
+
+def botTalkToGroup(botID,message,attach):
+    client.bots.post(botID,message,attach)
+
+
+
+def main():
+    print("     == sum.Pal Bot ==       \n\n")
+
+    groups = list(client.groups.list_all())
+    for group in groups:
+        print(group.name)
+
+    # BOT TALKING TO GROUP #
+    botTalkToGroup("22482b2e8f82a16f13242f93d8","david aint comin",[])
+
+
+
+
+if __name__ == "__main__":
+    main()

1d34a35d527701696af2d9e225f1628e75c79547
Jacob Jurilla <iamjake@protonmail.com>
2018-04-14 15:11:58 -0400

    Revisions

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -5,9 +5,21 @@ import groupy
 
 from groupy.client import Client
 
-token = "Insert Access Token Here"
+# Access Token to use bot #
+token = ""
+
+# bot : Not Evil Bot #
+bot_ID = ""
+
+# This is for the group chat : bots Taking Over The World #
+group_ID = 
 
 client = Client.from_token(token)
 
-for group in client.groups.list_all():
-   print(group.name)
+#for group in client.groups.list_all():
+#   print(group.name)
+
+test_Chat = client.groups.get(group_ID)
+#evil_Bot = client.bots.get(bot_ID)
+
+#message = test_Chat.post(text)

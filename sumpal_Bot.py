edf6a9c4894578954c337eefc604d69d76a26734
Jacob Jurilla <iamjake@protonmail.com>
2018-04-14 13:56:22 -0400

    Update to sumpal_Bot.py

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -2,3 +2,12 @@ import os
 import sys
 import json
 import groupy
+
+from groupy.client import Client
+
+token = "Insert Access Token Here"
+
+client = Client.from_token(token)
+
+for group in client.groups.list_all():
+   print(group.name)

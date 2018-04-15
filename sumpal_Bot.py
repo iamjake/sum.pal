ab2b363b5792a046e7d207e6db670b898384def1
Jacob Jurilla <iamjake@protonmail.com>
2018-04-14 20:32:29 -0400

    Revised structure.

--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -7,6 +7,7 @@ import time
 
 from groupy.client import Client
 from groupy.api import messages
+from groupy.api import attachments
 
 time.ctime()
 
@@ -17,16 +18,19 @@ token = ""
 bot_ID = ""
 
 # This is for the group chat : bots Taking Over The World #
-group_ID = 
+group_ID =
+
+
+type = 'mentions'
 
 # Defines Full Access #
 client = Client.from_token(token)
 
-groupMessages = groupy.api.messages.Messages(client.session,group_ID)
+userID = groupy.api.attachments.Mentions('mentions', user_id)
 
-# FUNC:
-# DETAILS:
-# NOTES:
+messageLimit = 4
+
+
 def botInitialPost(bot_ID, message):
     client.bots.post(bot_ID, message)
 
@@ -36,19 +40,17 @@ def botInitialPost(bot_ID, message):
 #          MESSAGE to the item bot wants to convey
 #          ATTACH to the images you would like to display alongside message
 # NOTES: This will be removed later on
-def botTalkToGroup(bot_ID, message,attach):
+def botTalksWhenCalled(bot_ID, message,attach):
     client.bots.post(bot_ID, message)
+
 
 def main():
     groupMessages = groupy.api.messages.Messages(client.session,group_ID)
     print("\t\t**Sum.Pal Bot Starting**\n\n")
     botInitialPost(bot_ID,"The Evil Bot has started, current time")
     botInitialPost(bot_ID, str(time.ctime()))
-        # for messageText in groupMessages.list():
-        # print(messageText.text)
     
     recentMessageID = 0
-
     for messageText in groupMessages.list():
         recentMessageID = messageText.id
         break
@@ -56,14 +58,14 @@ def main():
     i = 0
     while(1):
         for messageText in groupMessages.list_since(recentMessageID):
-            if "@bot" in messageText.text:
-                print(" - @bot recieved")
-                botTalkToGroup(bot_ID, "I heard you!", [])
+            if "@sum.pal " in messageText.text:
+                print("Bot was mentioned.\n")
+                botTalksWhenCalled(bot_ID, "I heard you!", [])
             recentMessageID = messageText.id
             i += 1
-            if i == 4:
+            if i == messageLimit:
                 for messageText in groupMessages.list(limit=i):
-                    print(messageText.text)
+                    print("Message from user {} contains {}".format(userID.user_id, messageText.text))
                 i = 0
             print(i)
 

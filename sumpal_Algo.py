bb6035058fda791a3568eb2713fd3634443e5fa2
AnthonyLSee <as2242@scarletmail.rutgers.edu>
2018-04-15 08:29:32 -0400

    Updated Bot, add text reformtter func

--- sumpal_Algo.py
+++ sumpal_Algo.py
@@ -129,6 +129,9 @@ def summaryMake( content ):
     print("Summary Length %s" % (len(summary)))
     print("Summary Ratio: %s" % (100 - (100 * (len(summary) / (len(title) + len(content))))))
     print(" __________________________________")
+
+    if len(summary) == 0:
+        return -1
     return summary
 
 
--- sumpal_Bot.py
+++ sumpal_Bot.py
@@ -15,7 +15,6 @@ time.ctime()
 # Access Token to use bot #
 token = "dnQ98hYoQQjFlo5sYAN4mqoCTYLH0V0Ye8cOGvM9"
 
-
 # bot : Not Evil Bot #
 bot_ID = "22482b2e8f82a16f13242f93d8"
 bot_name = "Not Evil Bot"
@@ -26,24 +25,25 @@ group_ID = 40061967
 # Defines Full Access #
 client = Client.from_token(token)
 
-
 for group in client.groups.list_all():
     print(group.name)
 userID = groupy.api.attachments.Mentions('mentions', '666')
 
 messageLimit = 10
 
-messageList = [ ]
+messageList = []
 
-groupMessages = groupy.api.messages.Messages(client.session,group_ID)
+groupMessages = groupy.api.messages.Messages(client.session, group_ID)
+
 
 # FUNC: botTalkToGroup
 # DETAILS: ID to the specific chat
 #          MESSAGE to the item bot wants to convey
 #          ATTACH to the images you would like to display alongside message
 # NOTES: This will be removed later on
-def botTalksWhenCalled(bot_ID, message,attach):
+def botTalksWhenCalled(bot_ID, message, attach):
     client.bots.post(bot_ID, message, attach)
+
 
 # FUNC: specificBotCall()
 # DETAILS: This function will execute if called by user
@@ -56,63 +56,84 @@ def specificBotCall():
             strHldr = messageText.text + " " + strHldr + " "
             print(strHldr)
 
+
+def reformatText(username, summaryText, format):
+    textHeader = ""
+
+    summaryText = summaryText.replace("@sum.pal", "")
+    summaryText = summaryText.replace("\n", "")
+
+    if format == "cmd":
+        textHeader = ("Here is a recap %s :\n" % (username))
+
+    if format == "limit":
+        textHeader = ("Here what I got so far : \n")
+
+    reformatString = textHeader + summaryText
+    return reformatString
+
+
 def main():
-    groupMessages = groupy.api.messages.Messages(client.session,group_ID)
+    groupMessages = groupy.api.messages.Messages(client.session, group_ID)
     print("\t\t**Sum.Pal Bot Starting**\n\n")
-    
+
     recentMessageID = 0
     for messageText in groupMessages.list():
         recentMessageID = messageText.id
         break
-    
+
     i = 0
     cmdIter = 0
-    while(1):
+    while (1):
         for messageText in groupMessages.list_since(recentMessageID):
             if messageText.name == bot_name:
                 break
+            currentUser = messageText.name
             strHolder = ""
+
             # BOT CMD SUMMARY #
             if "@sum.pal" in messageText.text:
                 print(" ======== @@@@@ ========")
-                for messageText in groupMessages.list_since(recentMessageID,limit = 1):
-                    for messageText in groupMessages.list(limit=messageLimit):
-                        if cmdIter == messageLimit:
-                            break
+                # for messageText in groupMessages.list_since(recentMessageID,limit = 1):
+                for messageText in groupMessages.list(limit=messageLimit):
+                    if cmdIter == messageLimit:
+                        break
+
+                    # Do not pick up bot text #
+                    if messageText.name != bot_name:
                         strHolder = messageText.text + " " + strHolder + " "
-                cmdIter += 1
+                        cmdIter += 1
+
+                # Build Summary #
+                summaryText = summaryMake(strHolder)
+                if summaryText == -1:
+                    summaryText = "No summary so far..."
+                finalText = reformatText(currentUser, summaryText, "cmd")
+                botTalksWhenCalled(bot_ID, finalText, [])
+                cmdIter = 0
+                i = 0
+
+            # MESSAGE LIMIT SUMMARY #
+            if i == messageLimit:
+                print(" =========== limit ========")
+                for messageText in groupMessages.list(limit=i):
+                    # Do not pick up bot text #
+                    if messageText.name != bot_name:
+                        strHolder = messageText.text + " " + strHolder + " "
+
+                # Build Summary #
                 print(strHolder)
                 summaryText = summaryMake(strHolder)
                 if summaryText == -1:
                     summaryText = "No summary so far..."
-                summaryText = summaryText.replace("@sum.pal","")
-                botTalksWhenCalled(bot_ID, summaryText, [])
-                cmdIter = 0
-                #       specificBotCall()
-                # botTalksWhenCalled(bot_ID, "I heard you!", [])
-                # for messageText in groupMessages.list_all_after(recentMessageID,limit = 1):
-                #    if "@sum.pal" in messageText.text:
-                #        specificBotCall()
-                #        botTalksWhenCalled(bot_ID, "I heard you!", [])
-                #            recentMessageID = messageText.id
-                
-                # MESSAGE LIMIT SUMMARY #
+                finalText = reformatText(currentUser, summaryText, "limit")
+                botTalksWhenCalled(bot_ID, finalText, [])
                 i = 0
-            if i == messageLimit:
-                print(" =========== limit ========")
-                for messageText in groupMessages.list(limit=i):
-                    strHolder = messageText.text + " " + strHolder + " "
-                summaryText = summaryMake(strHolder)
-                if summaryText == -1:
-                    summaryText = "No summary so far..."
-                summaryText = summaryText.replace("@sum.pal","")
-                summaryText = summaryText.replace("\n","")
-                botTalksWhenCalled(bot_ID, summaryText, [])
-                print(summaryText)
-                i = 0
-    print(i)
-        i += 1
+
+            print(i)
+            i += 1
             recentMessageID = messageText.id
+
 
 if __name__ == "__main__":
     main()
